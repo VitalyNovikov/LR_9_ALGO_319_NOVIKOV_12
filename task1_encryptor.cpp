6e105504cc8e2f0f3772f15906764a18a4acae6b
VitalyNovikov <134317908+VitalyNovikov@users.noreply.github.com>
2025-03-24 12:52:23 +0200

    Update task1_encryptor.cpp

--- task1_encryptor.cpp
+++ task1_encryptor.cpp
@@ -2,9 +2,11 @@
 
 #include "task1_encryptor.h" // Підключаємо заголовковий файл
 
+using namespace std;
+
 // Функція для шифрування тексту методом Цезаря
-std::string encryptCaesar(const std::string& text, int shift) {
-    std::string result = text; // Копіюємо вхідний текст
+string encryptCaesar(const string& text, int shift) {
+    string result = text; // Копіюємо вхідний текст
     
     // Проходимо по кожному символу рядка
     for (char& c : result) {
@@ -18,6 +20,6 @@ std::string encryptCaesar(const std::string& text, int shift) {
 }
 
 // Функція для розшифрування тексту методом Цезаря
-std::string decryptCaesar(const std::string& text, int shift) {
+string decryptCaesar(const string& text, int shift) {
     return encryptCaesar(text, 26 - shift); // Використовуємо зворотне зміщення
 }
